73969abbc9111593ddac6c4ac8f234044c1ed612
Okan Sahin <okan.shn@hotmail.com>
2022-01-31 18:12:22 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Try_Catch_Finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz: ");
+                int girilenSayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı:  "+girilenSayi);
+            }
+
+            catch(Exception ex)
+            {
+                    Console.WriteLine("Hata"+ ex.Message.ToString());
+            }
+
+            finally
+            {
+
+            }
+        }
+    }
+}
